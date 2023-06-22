210c300df1f8a0cc41f73b72c753a3d55e90bc65
bk-gill <132428715+bk-gill@users.noreply.github.com>
2023-06-22 14:20:53 -0400

    Added resume and fixed formatting

--- pages/resume.js
+++ pages/resume.js
@@ -1,12 +1,29 @@
+import Head from 'next/head';
 import Header from '../components/Header';
 
-function Resume() {
+export default function Resume() {
   return (
     <div>
-      <Header />
-      <h1>Resume</h1>
-    </div>
-  )
-}
+      <Head>
+        <title>Resume | Your Name</title>
+      </Head>
 
-export default Resume;
+      <Header />
+
+      <div style={{ textAlign: 'center' }}>
+        <h1 style={{ marginBottom: '2rem' }}>Resume</h1>
+      </div>
+
+      <div style={{ width: '80%', margin: '0 10%' }}>
+        <iframe
+          src="https://drive.google.com/file/d/1y32g6vIXBuJ8f9NCM6wtjnjojYVpJV5m/preview"
+          width="100%"
+          height="800px"
+          style={{ border: '1px solid white' }}
+        />
+      </div>
+
+      {/* Additional content for your resume page */}
+    </div>
+  );
+}
