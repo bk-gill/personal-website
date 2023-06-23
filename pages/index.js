718e86803df6832ded3e988e7025b34ced265ecc
bk-gill <132428715+bk-gill@users.noreply.github.com>
2023-06-22 21:47:21 -0400

    Fixed formatting of header

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>balkarn's Portfolio</title>
+        <title>Balkarn Vick Gill</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
