b8713bd843b0a8c9825978db60c345a391c7ca41
bk-gill <132428715+bk-gill@users.noreply.github.com>
2023-07-20 00:23:26 -0600

    Added resume link

--- pages/resume.js
+++ pages/resume.js
@@ -15,7 +15,7 @@ export default function Resume() {
       </div>
       <div style={{ width: '80%', margin: '0 10%' }}>
         <iframe
-          src="https://drive.google.com/file/d/1bNXhRJU-vsxHi4f-nRncNOn-K84ejdMu/preview"
+          src="https://drive.google.com/file/d/1Ze0w6CBE4zkj9p-NjFwJc9AFpFnKOM6S/view"
           width="100%"
           height="800px"
           style={{ border: '1px solid white' }}
