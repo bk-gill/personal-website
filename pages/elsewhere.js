dadade364eab4379bb463b0ffe825fbac8b2cb47
bk-gill <132428715+bk-gill@users.noreply.github.com>
2023-07-28 20:17:25 -0600

    Added picture of myself to about section

--- pages/elsewhere.js
+++ pages/elsewhere.js
@@ -14,8 +14,19 @@ export default function Elsewhere() {
         <h1 style={{ marginBottom: '30px' }}>about</h1>
 
         <div className="textbox" style={{ maxWidth: '600px', margin: '0 auto' }}>
-        <p style={{ marginBottom: '10px', fontSize: '18px'}}>Hi, I'm Balkarn! I'm currently a Master's student in Electrical and Computer Engineering with a specialization in Software Engineering.</p>
-        <p style={{ marginBottom: '10px', fontSize: '18px'}}>I will be using this website to post new updates regarding projects and myself while I pursue my passion of software! </p>
+          {/* Paragraphs containing information about Balkarn */}
+          <p style={{ marginBottom: '10px', fontSize: '18px' }}>
+            Hi, I'm Balkarn! I'm currently a Master's student in Electrical and Computer Engineering with a specialization in Software Engineering.
+          </p>
+          <p style={{ marginBottom: '10px', fontSize: '18px' }}>
+            I will be using this website to post new updates regarding projects and myself while I pursue my passion for software!
+          </p>
+
+          <img
+            src="/IMG_0048.png" 
+            alt="Balkarn's Picture"
+            style={{ maxWidth: '300px', margin: '20px auto' }}
+          />
         </div>
       </div>
     </div>
