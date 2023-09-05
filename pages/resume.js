6fb0fb552478ec5a7021d7dd43f8f91b94223092
bk-gill <132428715+bk-gill@users.noreply.github.com>
2023-09-05 14:54:41 -0600

    Minor style changes

--- pages/resume.js
+++ pages/resume.js
@@ -15,7 +15,7 @@ export default function Resume() {
       </div>
       <div style={{ width: '80%', margin: '0 10%' }}>
         <iframe
-          src="https://drive.google.com/file/d/1a6vOvyTU5-6550OpQNwJUP16qEWkte4Z/preview"
+          src="https://drive.google.com/file/d/1LQyJQ5eFm2-f_rraO6OWZ3SymV9IgtAu/preview"
           width="100%"
           height="800px"
           style={{ border: '1px solid white' }}
