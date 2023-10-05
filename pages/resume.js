1660d3b2bc454fc64b45eedf709794a7c4901aa5
bk-gill <132428715+bk-gill@users.noreply.github.com>
2023-10-05 14:27:33 -0600

    Minor styling changes

--- pages/resume.js
+++ pages/resume.js
@@ -1,5 +1,5 @@
-import Head from 'next/head';
-import Header from '../components/Header';
+import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Resume() {
   return (
@@ -10,20 +10,17 @@ export default function Resume() {
 
       <Header />
 
-      <div style={{ textAlign: 'center', marginTop: '80px'  }}>
-        <h1 style={{ marginBottom: '2rem' }}>resume</h1>
+      <div style={{ textAlign: "center", marginTop: "80px" }}>
+        <h1 style={{ marginBottom: "2rem" }}>resume</h1>
       </div>
-      <div style={{ width: '80%', margin: '0 10%' }}>
+      <div style={{ width: "80%", margin: "0 10%" }}>
         <iframe
-          src="https://drive.google.com/file/d/1BYt5OBIP8sIfSstdSzd4yX8Po4M3qrVn/preview"
+          src="https://drive.google.com/file/d/1qf4RJ8-jCXyzl_wMHtBEZaFUN9CNASJi/preview"
           width="100%"
           height="800px"
-          style={{ border: '1px solid white' }}
+          style={{ border: "1px solid white" }}
         />
       </div>
-
-
-      
     </div>
   );
 }
