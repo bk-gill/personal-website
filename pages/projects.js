c190c6b4ed078b7b147bea7a2afbfd075364b9c9
bk-gill <132428715+bk-gill@users.noreply.github.com>
2023-12-27 15:48:08 -0500

    Added projects

--- pages/projects.js
+++ pages/projects.js
@@ -1,28 +1,37 @@
-import React, { useState } from 'react';
-import Head from 'next/head';
-import Header from '../components/Header';
+import React, { useState } from "react";
+import Head from "next/head";
+import Header from "../components/Header";
 
 function Projects() {
   const projects = [
     {
-      title: 'Autonomous Plant Disease Detection Pathfinder',
-      imageSrc: ['/pathfinder1.jpg', '/pathfinder2.jpg'],
-      description: '- Coordinated a team of interdisciplinary engineers in building an automated pathfinder which was programmed to weave through rows of plants on its own, avoiding obstacles and detecting signs of disease in plants.\n\n- Temperature sensor, humidity sensor, and ultrasonic distance sensor were wired and programmed using Arduino, a hyperspectral camera was also wired into the design and programmed using C via a Raspberry Pi, and important data points were transmitted using a Wi-Fi module.\n\n🏆 Won first place at the F22 bioinstrumentation design competition! 🏆',
+      title: "Neo Financial's Tech Thursdays Web Application",
+      imageSrc: ["/nfwa1.png"],
+      description:
+        "- Worked with a team of software developers to create a web application for Neo Financial's weekly Tech Thursdays events.\n\n- Utiltized Figma to design the website, React for front-end development, and Drupal for back-end.\n\n- Ownership was transferred to Neo Financial, who are currently responsible for maintaining the web app.",
     },
     {
-      title: 'This Website',
-      imageSrc: ['/website.png'],
-      description: '- Created this website as a way to learn the React framework Next.js.\n\n- Initial designs were created using Sketch, features were implemented using Javascript. \n\n- Integrated with Git version control and deployed the website on Vercel for continuous deployment.',
+      title: "Autonomous Plant Disease Detection Pathfinder",
+      imageSrc: ["/pathfinder1.jpg", "/pathfinder2.jpg"],
+      description:
+        "- Coordinated a team of interdisciplinary engineers in building an automated pathfinder which was programmed to weave through rows of plants on its own, avoiding obstacles and detecting signs of disease in plants.\n\n- Temperature sensor, humidity sensor, and ultrasonic distance sensor were wired and programmed using Arduino, a hyperspectral camera was also wired into the design and programmed using C via a Raspberry Pi, and important data points were transmitted using a Wi-Fi module.\n\n🏆 Won first place at the F22 bioinstrumentation design competition! 🏆",
     },
     {
-      title: 'French Bulldog CT-Scan Apparatus',
-      imageSrc: ['/bulldog1.png','/bulldog2.png','/bulldog3.png'],
-      description: '- Team chosen out of 60+ applicants to design product for Dr. Alex Zur Linden from the Ontario Veterinary College.\n\n- Purpose of the project was to create an apparatus that reduces visibility obstructions during CT-Scans for Brachycephalic dog breeds.\n\n- Extensive research into material selection as apparatus must be constructed without using any metals (and any other materials that interfere with CT-Scan imaging) while being able to withstand all relevant forces.\n\n- SolidWorks and ANSYS were utilized to create a fully-working model that met all constraints and criteria of the client.',
+      title: "This Website",
+      imageSrc: ["/website.png"],
+      description:
+        "- Created this website as a way to learn the React framework Next.js.\n\n- Initial designs were created using Sketch, features were implemented using Javascript. \n\n- Integrated with Git version control and deployed the website on Vercel for continuous deployment.",
     },
     {
-      title: 'Current Project',
-      imageSrc: ['/p4.png'],
-      description: '- Currently working on an iOS application\n\n- Stay tuned!',
+      title: "French Bulldog CT-Scan Apparatus",
+      imageSrc: ["/bulldog1.png", "/bulldog2.png", "/bulldog3.png"],
+      description:
+        "- Team chosen out of 60+ applicants to design product for Dr. Alex Zur Linden from the Ontario Veterinary College.\n\n- Purpose of the project was to create an apparatus that reduces visibility obstructions during CT-Scans for Brachycephalic dog breeds.\n\n- Extensive research into material selection as apparatus must be constructed without using any metals (and any other materials that interfere with CT-Scan imaging) while being able to withstand all relevant forces.\n\n- SolidWorks and ANSYS were utilized to create a fully-working model that met all constraints and criteria of the client.",
+    },
+    {
+      title: "Current Project",
+      imageSrc: ["/p4.png"],
+      description: "- Currently working on an iOS application\n\n- Stay tuned!",
     },
   ];
 
@@ -48,8 +57,8 @@ function Projects() {
 
       <Header />
 
-      <div style={{ textAlign: 'center', marginTop: '80px' }}>
-        <h1 style={{ marginBottom: '30px' }}>projects</h1>
+      <div style={{ textAlign: "center", marginTop: "80px" }}>
+        <h1 style={{ marginBottom: "30px" }}>projects</h1>
 
         <div style={projectBoxStyle}>
           <h2>{currentProject.title}</h2>
@@ -63,7 +72,7 @@ function Projects() {
               />
             ))}
           </div>
-          <p style={{ whiteSpace: 'pre-line' }}>{currentProject.description}</p>
+          <p style={{ whiteSpace: "pre-line" }}>{currentProject.description}</p>
         </div>
 
         <div style={navigationButtonsStyle}>
@@ -76,28 +85,28 @@ function Projects() {
 }
 
 const projectBoxStyle = {
-  borderRadius: '8px',
-  padding: '20px',
-  maxWidth: '600px',
-  margin: '0 auto',
+  borderRadius: "8px",
+  padding: "20px",
+  maxWidth: "600px",
+  margin: "0 auto",
 };
 
 const projectImagesContainerStyle = {
-  display: 'flex',
-  justifyContent: 'center',
-  gap: '10px',
-  marginTop: '20px',
+  display: "flex",
+  justifyContent: "center",
+  gap: "10px",
+  marginTop: "20px",
 };
 
 const projectImageStyle = {
-  width: '50%',
-  height: 'auto',
-  marginBottom: '20px',
-  objectFit: 'cover',
+  width: "50%",
+  height: "auto",
+  marginBottom: "20px",
+  objectFit: "cover",
 };
 
 const navigationButtonsStyle = {
-  marginTop: '20px',
+  marginTop: "20px",
 };
 
 export default Projects;
